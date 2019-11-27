75f15eaac5b3992d32320f7e07e2cae48cecca4c
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-11-27 16:57:43 +0530

    [Dipali|Shyam] Fix.removed unnessary annotations

--- src/main/java/com/thoughtworks/payment/BankDetails.java
+++ src/main/java/com/thoughtworks/payment/BankDetails.java
@@ -1,25 +1,20 @@
 package com.thoughtworks.payment;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public class BankDetails {
-
-    @Id
     private int id;
     private String name;
     private long accountNumber;
     private String ifscCode;
 
-    public BankDetails(int id,String name, int accountNumber, String ifscCode) {
-        this.id=id;
+    public BankDetails(int id, String name, int accountNumber, String ifscCode) {
+        this.id = id;
         this.name = name;
         this.accountNumber = accountNumber;
         this.ifscCode = ifscCode;
     }
 
-    public BankDetails(){}
+    public BankDetails() {
+    }
 
     public String getName() {
         return name;
