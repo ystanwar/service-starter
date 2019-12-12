c4105f7b4f7d0f7e921e65bbd6741ba8b5fdec24
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-12-12 12:14:41 +0530

    [Shyam|Dipali] Fix.transfer responsibility of checkdetails method to get response to another method

--- src/main/java/com/thoughtworks/bankclient/BankClient.java
+++ src/main/java/com/thoughtworks/bankclient/BankClient.java
@@ -2,10 +2,10 @@ package com.thoughtworks.bankclient;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
 
 @Service
 public class BankClient {
@@ -19,6 +19,10 @@ public class BankClient {
                 URLEncoder.encode(accountNumberQueryParameter, charset),
                 URLEncoder.encode(ifscCode, charset));
         URL connection = new URL(url + "?" + query);
+        return getResponseCode(connection);
+    }
+
+    private int getResponseCode(URL connection) throws IOException {
         HttpURLConnection httpURLConnection = (HttpURLConnection) connection.openConnection();
         httpURLConnection.setRequestProperty("Content-Type", "application/json");
         httpURLConnection.setRequestProperty("Accept", "application/json");
