0fba0eaf79dba72f5e456308023d252305754d56
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-12-12 12:22:32 +0530

    [Shyam|Dipali] Fix.transfer responsibility of checkFraud method to get response to another method

--- src/main/java/com/thoughtworks/fraudclient/FraudClient.java
+++ src/main/java/com/thoughtworks/fraudclient/FraudClient.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.thoughtworks.payment.Payment;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -15,15 +16,19 @@ public class FraudClient {
     public int checkFraud(Payment payment) throws Exception {
         String url = baseUrl + "/checkFraud";
         Gson gson = new Gson();
-        String json = gson.toJson(payment);
+        String paymentData = gson.toJson(payment);
         URL connection = new URL(url);
+        return getResponseCode(connection, paymentData);
+    }
+
+    private int getResponseCode(URL connection, String paymentData) throws IOException {
         HttpURLConnection httpURLConnection = (HttpURLConnection) connection.openConnection();
         httpURLConnection.setRequestProperty("Content-Type", "application/json");
         httpURLConnection.setRequestProperty("Accept", "application/json");
         httpURLConnection.setRequestMethod("POST");
         httpURLConnection.setDoOutput(true);
         OutputStreamWriter out = new OutputStreamWriter(httpURLConnection.getOutputStream());
-        out.write(json);
+        out.write(paymentData);
         out.close();
         return httpURLConnection.getResponseCode();
     }
