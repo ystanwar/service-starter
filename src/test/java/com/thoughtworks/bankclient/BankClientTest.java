368e4382a29ab99c20a74a2ffdd037ca9f8806a4
shyamkondisetty <shyamkondisetty@gmail.com>
2019-12-12 16:16:29 +0530

    [Shyam|Dipali] Refactor. add mocking to external services

--- src/test/java/com/thoughtworks/bankclient/BankClientTest.java
+++ src/test/java/com/thoughtworks/bankclient/BankClientTest.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.bankclient;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class BankClientTest {
@@ -14,11 +16,23 @@ public class BankClientTest {
 
     @Test
     public void checkBankDetails() throws Exception {
+        WireMockServer wireMockServer = new WireMockServer(8082);
+        wireMockServer.start();
+        configureFor("localhost", 8082);
+
+        stubFor(get(urlEqualTo("/checkDetails?accountNumber=12345&&ifscCode=HDFC1234")).willReturn(aResponse().withStatus(200)));
         assertEquals(200, bankClient.checkBankDetails(12345, "HDFC1234"));
+        wireMockServer.stop();
     }
 
     @Test
     public void failsToCheckBankDetails() throws Exception {
+        WireMockServer wireMockServer = new WireMockServer(8082);
+        wireMockServer.start();
+        configureFor("localhost", 8082);
+
+        stubFor(get(urlEqualTo("/checkDetails?accountNumber=0000&&ifscCode=HDFC1234")).willReturn(aResponse().withStatus(404)));
         assertEquals(404, bankClient.checkBankDetails(0000, "HDFC1234"));
+        wireMockServer.stop();
     }
 }
--- src/test/java/com/thoughtworks/fraudclient/FraudClientTest.java
+++ src/test/java/com/thoughtworks/fraudclient/FraudClientTest.java
@@ -1,11 +1,13 @@
 package com.thoughtworks.fraudclient;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.thoughtworks.payment.BankDetails;
 import com.thoughtworks.payment.Payment;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -16,10 +18,17 @@ public class FraudClientTest {
 
     @Test
     public void checkFraud() throws Exception {
+        WireMockServer wireMockServer = new WireMockServer(8083);
+        wireMockServer.start();
+        configureFor("localhost", 8083);
+
+        stubFor(post(urlEqualTo("/checkFraud")).willReturn(aResponse().withStatus(200)));
+
         BankDetails beneficiary = new BankDetails("user1", 12345, "HDFC1234");
         BankDetails payee = new BankDetails("user2", 67890, "HDFC1234");
-
         Payment payment = new Payment(100, beneficiary, payee);
+
         assertEquals(200, fraudClient.checkFraud(payment));
+        wireMockServer.stop();
     }
 }
