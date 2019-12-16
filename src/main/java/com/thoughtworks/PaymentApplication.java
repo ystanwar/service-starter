27650963c75695f533eda49579050bcf3ed37db2
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-12-16 10:19:57 +0530

    [Shyam|Dipali] Add. configuration file for database connection

--- src/main/java/com/thoughtworks/PaymentApplication.java
+++ src/main/java/com/thoughtworks/PaymentApplication.java
@@ -1,9 +1,13 @@
 package com.thoughtworks;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
