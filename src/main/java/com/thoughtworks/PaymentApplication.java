ce84cec3f2560a5b998d03b151eaa5525429f4ae
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-12-17 14:26:31 +0530

    [Shyam|Dipali] Fix.Changed environments.properties to environments.yml

--- src/main/java/com/thoughtworks/PaymentApplication.java
+++ src/main/java/com/thoughtworks/PaymentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:environments.properties")
+@PropertySource("classpath:environments.yml")
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
