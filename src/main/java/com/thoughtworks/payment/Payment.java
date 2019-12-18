ab68e4a18e1dd40123cac45040864278c451bb04
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-12-18 16:25:11 +0530

    [Shyam|Dipali] Fix.removed id from request

--- src/main/java/com/thoughtworks/payment/Payment.java
+++ src/main/java/com/thoughtworks/payment/Payment.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.payment;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
 public class Payment {
     @Id
+    @JsonIgnore
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
