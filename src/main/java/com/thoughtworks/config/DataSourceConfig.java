ecbc08418c70162d72e47c0fdcac960498cb2859
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2020-01-08 16:09:48 +0530

    [Shyam|Dipali] Add. externalizing the configuration files using custom class

---
+++ src/main/java/com/thoughtworks/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("postgres");
+        return dataSourceBuilder.build();
+    }
+
+}
