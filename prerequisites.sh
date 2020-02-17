812646b1d070fd06c4c2232d55a2abe3ef515864
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2020-02-17 18:34:09 +0530

    [Shyam|Dipali] Add. prerequisites checker file and add prometheus in filter

---
+++ prerequisites.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# shellcheck disable=SC2006
+JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{print $1}'`
+NOT_FOUND="prerequestives.sh:"
+if [ "$JAVA_VERSION" == "$NOT_FOUND" ]
+then
+    echo "install java"
+else
+    echo "java is already installed"
+fi
+DOCKER_VERSION=`docker --version 2>&1 |awk 'NR==1{print $1}'`
+if [ "$DOCKER_VERSION" == "$NOT_FOUND" ]
+then
+    echo "install docker"
+else
+    echo "docker is already installed"
+fi
+POSTGRES_VERSION=`psql --version 2>&1 |awk 'NR==1{ print $1}'`
+if [ "$POSTGRES_VERSION" == "$NOT_FOUND" ]
+then
+    echo "install postgres"
+else
+    echo "postgres is already installed"
+fi
--- src/main/java/com/thoughtworks/filter/ServletContextFilter.java
+++ src/main/java/com/thoughtworks/filter/ServletContextFilter.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.filter;
 
+import com.thoughtworks.prometheus.Prometheus;
+import io.micrometer.core.instrument.Counter;
+import io.prometheus.client.Gauge;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ServletContextRequestLoggingFilter;
@@ -11,6 +15,9 @@ import java.util.UUID;
 
 @Configuration
 public class ServletContextFilter implements Filter {
+    @Autowired
+    private Prometheus prometheus;
+
     @Bean
     public ServletContextRequestLoggingFilter logFilter() throws ServletException {
         ServletContextRequestLoggingFilter filter
@@ -25,6 +32,17 @@ public class ServletContextFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         MDC.put("request.id", String.valueOf(UUID.randomUUID()));
-        filterChain.doFilter(servletRequest, servletResponse);
+        Gauge paymentRequestTime;
+        Counter paymentsCounter;
+        paymentsCounter = prometheus.getPaymentsCounter();
+        paymentsCounter.increment();
+        paymentRequestTime = prometheus.getPaymentRequestTime();
+        Gauge.Timer timer = paymentRequestTime.startTimer();
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        } finally {
+            timer.setDuration();
+        }
     }
 }
