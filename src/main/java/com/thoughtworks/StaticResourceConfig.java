d337e1a05dc4c67882d659be22f515ae0bc327b9
shyamkondisetty <shyamkondisetty@gmail.com>
2020-03-23 09:16:57 +0530

    [Shyam] Refactor. added swagger config along with internal swagger ui config

--- src/main/java/com/thoughtworks/StaticResourceConfig.java
+++ src/main/java/com/thoughtworks/StaticResourceConfig.java
@@ -6,15 +6,19 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class StaticResourceConfig extends WebMvcConfigurationSupport {
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/static/" };
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+
+        registry
+                .addResourceHandler("/**")
                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
+
 }
