0b73854edf4b098d7fc760292e149e56bee72afc
karthik.86@live.com <contributE@08>
2020-03-31 17:06:17 +0530

    Added condition to execute request and response logging logic only when INFO level is configured.

--- src/main/java/com/thoughtworks/filter/CustomRequestLoggingFilter.java
+++ src/main/java/com/thoughtworks/filter/CustomRequestLoggingFilter.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.slf4j.MDC;
@@ -20,6 +22,7 @@ import java.util.UUID;
 
 import static net.logstash.logback.argument.StructuredArguments.v;
 
+@Slf4j
 @Service
 @Order(2)
 public class CustomRequestLoggingFilter implements Filter {
@@ -31,47 +34,46 @@ public class CustomRequestLoggingFilter implements Filter {
         ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
 
         MDC.put("request.id", String.valueOf(UUID.randomUUID()));
-        MDC.put("parentrequest.id", requestWrapper.getHeader("PARENT_REQ_ID"));
+        MDC.put("parentrequest.id", ((HttpServletRequest) servletRequest).getHeader("PARENT_REQ_ID"));
         try {
             filterChain.doFilter(requestWrapper, responseWrapper);
         } finally {
-            String requestBody = new String(requestWrapper.getContentAsByteArray());
-            String responseBody = new String(responseWrapper.getContentAsByteArray());
-
-            responseBody = isResponseLarge(responseBody);
-
-            requestBody = isRequestLarge(requestBody);
-
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-
-            Map<String, String> headerMap = new HashMap<>();
-
-            Enumeration en = (httpServletRequest).getHeaderNames();
-            while (en.hasMoreElements()) {
-                String headerName = (String) en.nextElement();
-                headerMap.put(headerName, httpServletRequest.getHeader(headerName));
+            if(log.isInfoEnabled()){
+                logRequestAndResponse((HttpServletRequest)servletRequest,(HttpServletResponse)servletResponse);
             }
-
-            Map<String, String[]> requestParams = servletRequest.getParameterMap();
-            ObjectMapper objectMapper = new ObjectMapper();
-            String paramString = objectMapper.writeValueAsString(requestParams);
-            String headerString = objectMapper.writeValueAsString(headerMap);
-
-            logger.info("{eventCode:{},description:{},details:{headers:{},params:{},body:{}},exception:{}}", v("eventCode", "REQUEST_RECEIVED"), v("description", httpServletRequest.getMethod() + " " + httpServletRequest.getServletPath()), v("headers", headerString), v("params", paramString), v("body", requestBody), v("exception", ""));
-            logger.info("{eventCode:{},description:{},details:{statusCode:{},headers:{},body:{}},exception:{}}", v("eventCode", "RESPONSE_SENT"), v("description", httpServletRequest.getMethod() + " " + httpServletRequest.getServletPath()), v("statusCode", httpServletResponse.getStatus()), v("headers", headerString), v("body", responseBody), v("exception", ""));
-
             MDC.clear();
             responseWrapper.copyBodyToResponse();
         }
     }
 
-    private String isResponseLarge(String responseBody) {
+    private void logRequestAndResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws JsonProcessingException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(httpServletResponse);
+        String requestBody = new String(requestWrapper.getContentAsByteArray());
+        String responseBody = new String(responseWrapper.getContentAsByteArray());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        log.info("{\"eventCode\":{},\"description\":{},\"details\":{\"headers\":{},\"params\":{},\"body\":{}}}", v("\"eventCode\"", "\"REQUEST_RECEIVED\""), v("\"description\"", httpServletRequest.getMethod() + " " + httpServletRequest.getServletPath()), v("\"headers\"", objectMapper.writeValueAsString(getHeaders(httpServletRequest))), v("\"params\"", objectMapper.writeValueAsString(httpServletRequest.getParameterMap())), v("\"body\"", requestBody));
+        log.info("{\"eventCode\":{},\"description\":{},\"details\":{\"statusCode\":{},\"headers\":{},\"body\":{}}}", v("\"eventCode\"", "\"RESPONSE_SENT\""), v("\"description\"", httpServletRequest.getMethod() + " " + httpServletRequest.getServletPath()), v("\"statusCode\"", httpServletResponse.getStatus()), v("\"headers\"", objectMapper.writeValueAsString(getHeaders(httpServletRequest))), v("\"body\"", getResponseContent(responseBody)));
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest httpServletRequest){
+        Map<String, String> headerMap = new HashMap<>();
+        Enumeration en = httpServletRequest.getHeaderNames();
+        while (en.hasMoreElements()) {
+            String headerName = (String) en.nextElement();
+            headerMap.put(headerName, httpServletRequest.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getResponseContent(String responseBody) {
         if (responseBody.length() > 1000) responseBody = "\"payload is too large to log\"";
         return responseBody;
     }
 
-    private String isRequestLarge(String requestBody) {
+    private String getRequestContent(String requestBody) {
         if (requestBody.length() > 1000) requestBody = "\"payload is too large to log\"";
         return requestBody;
     }
