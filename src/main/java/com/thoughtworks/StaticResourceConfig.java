21060611acc9dcc4c8e99ef6c915449dd0fb0867
shyamkondisetty <shyamkondisetty@gmail.com>
2020-04-15 02:37:46 +0530

    [Shyam] Refactor. removed vulnerabilities from the dependencies

--- src/main/java/com/thoughtworks/StaticResourceConfig.java
+++ src/main/java/com/thoughtworks/StaticResourceConfig.java
@@ -1,30 +1,30 @@
-package com.thoughtworks;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-// @Configuration
-// public class StaticResourceConfig extends WebMvcConfigurationSupport {
-
-//     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-//             "classpath:/META-INF/resources/", "classpath:/static/" };
-
-//     @Override
-//     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//         super.addResourceHandlers(registry);
-
-//         registry
-//                 .addResourceHandler("/**")
-//                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-//     }
-
-//     @Bean
-//     public InternalResourceViewResolver defaultViewResolver() {
-//         return new InternalResourceViewResolver();
-//     }
-
-// }
+//package com.thoughtworks;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+//// @Configuration
+//// public class StaticResourceConfig extends WebMvcConfigurationSupport {
+//
+////     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+////             "classpath:/META-INF/resources/", "classpath:/static/" };
+//
+////     @Override
+////     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+////         super.addResourceHandlers(registry);
+//
+////         registry
+////                 .addResourceHandler("/**")
+////                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+////     }
+//
+////     @Bean
+////     public InternalResourceViewResolver defaultViewResolver() {
+////         return new InternalResourceViewResolver();
+////     }
+//
+//// }
